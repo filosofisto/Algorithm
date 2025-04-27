039fbc92ae33f4d2cc89b785d28b4f253b4d8528
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-04-27 10:20:49 +0200

    Fix for fastest false

--- C++/algoexpert/easy/TandemBibycle/main.cpp
+++ C++/algoexpert/easy/TandemBibycle/main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 
 int tandemBicycle(vector<int> redShirtSpeeds, vector<int> blueShirtSpeeds, bool fastest)
 {
-	sort(redShirtSpeeds.begin(), redShirtSpeeds.end(), greater<>()); 	// descendent	
+	if (fastest) {
+		sort(redShirtSpeeds.begin(), redShirtSpeeds.end(), greater<>()); 	// descendent	
+	} else {
+		sort(redShirtSpeeds.begin(), redShirtSpeeds.end());
+	}
 	sort(blueShirtSpeeds.begin(), blueShirtSpeeds.end());							// ascendent
 	auto length = redShirtSpeeds.size();
 	auto sum = 0;
@@ -22,10 +26,10 @@ int main()
 {
 	vector<int> red{5,5,3,9,2};
 	vector<int> blue{3,6,7,2,1};
-	auto fastest = true;
- 
-	cout << tandemBicycle(red, blue, fastest);
 
+	cout << "fastest: true " << " -> " << tandemBicycle(red, blue, true) << '\n';
+	cout << "fastest: false" << " -> " << tandemBicycle(red, blue, false) << '\n';
+	
 	return EXIT_SUCCESS;
 }
 
