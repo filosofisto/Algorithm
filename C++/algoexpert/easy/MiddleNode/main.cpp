4bad6cd59133b292af6bb7477b6ade305dfc256b
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-03 20:03:14 +0200

    Skeleton

--- C++/algoexpert/easy/MiddleNode/main.cpp
+++ C++/algoexpert/easy/MiddleNode/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+// This is an input struct. Do not edit.
+class LinkedList {
+ public:
+  int value;
+  LinkedList* next = nullptr;
+
+  LinkedList(int value) { this->value = value; }
+};
+
+LinkedList* middleNode(LinkedList* linkedList) {
+  // Write your code here.
+  return nullptr;
+}
+
 int main()
 {
 	return EXIT_SUCCESS;
