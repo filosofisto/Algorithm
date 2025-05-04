061a7f9f1595c3b61a18e82cd05f3d5deed27cb5
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 10:36:25 +0200

    Finish ProductSum

--- C++/algoexpert/easy/ProductSum/main.cpp
+++ C++/algoexpert/easy/ProductSum/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <any>
+#include <vector>
+#include <typeinfo>
+
+using namespace std;
+
+int productSum(vector<any> array, int depth)
+{
+	int sum = 0;
+
+	for (const auto& item : array) {
+		if (item.type() == typeid(int)) {
+			sum += any_cast<int>(item);
+		}	else if (item.type() == typeid(vector<any>)) {
+			sum += productSum(any_cast<vector<any>>(item), depth+1);
+		}
+	}
+
+	return depth * sum;
+}
+
+int productSum(vector<any> array)
+{
+	return productSum(array, 1);
+}
 
 int main()
 {
+	vector<any> array{5, 2, vector<any>{7, -1}, 3, vector<any>{6, vector<any>{-13, 8}, 4}};
+	cout << productSum(array);
 
+	return EXIT_SUCCESS;
 }
