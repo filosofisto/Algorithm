7dd97ab419fa4297a2a8105be2ca28f84ebbb5cc
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 17:01:11 +0200

    Fix

--- C++/algoexpert/easy/CaesarCypherEncryptor/main.cpp
+++ C++/algoexpert/easy/CaesarCypherEncryptor/main.cpp
@@ -5,15 +5,14 @@ using namespace std;
 string caesarCypherEncryptor(string str, int key) 
 {
   string result{str};
-	char newChar;
+	key = key % 26;
 
 	for (auto& ch : result) {
-		newChar = ch + key;
-		if (newChar > 122) {
-			newChar = 96 + newChar % 122;
-		}
-
-		ch = newChar;
+		ch = ch + key;
+		
+		if (ch > 122) {
+			ch = 96 + ch - 122;
+		}		
 	}
 
   return result;
@@ -21,7 +20,13 @@ string caesarCypherEncryptor(string str, int key)
 
 int main()
 {
-	cout << caesarCypherEncryptor("z", 1) << endl;
+
+	cout << caesarCypherEncryptor("xyz", 2) << endl;
+	cout << caesarCypherEncryptor("abc", 0) << endl;
+	cout << caesarCypherEncryptor("abc", 26) << endl;
+	cout << caesarCypherEncryptor("abc", 52) << endl;
+	cout << caesarCypherEncryptor("abc", 57) << endl;
+	cout << caesarCypherEncryptor("xyz", 25) << endl;
 
 	return EXIT_SUCCESS;
 }
