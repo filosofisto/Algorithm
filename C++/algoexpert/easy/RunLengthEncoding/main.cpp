95461d353806d1763ac3b1a92526be65aff3ffbe
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 21:24:38 +0200

    Fix

--- C++/algoexpert/easy/RunLengthEncoding/main.cpp
+++ C++/algoexpert/easy/RunLengthEncoding/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 string runLengthEncoding(string str) 
 {
+	/*
 	char curr = str[0];
 	size_t length = str.size();
 	int count = 1;
@@ -14,7 +15,7 @@ string runLengthEncoding(string str)
 		if (str[i] == curr) {
 			if (++count == 9) { // 9 is the max length
 				oss << count << curr;
-				count = 0;
+				count = 1;
 			}
 
 			if (i == (length - 1)) {
@@ -29,6 +30,37 @@ string runLengthEncoding(string str)
 				oss << count << curr;
 			}
 		}
+	}*/
+
+	size_t length = str.size();
+	int count = 1;
+	ostringstream oss;
+
+	for (size_t i{1}; i < length; ++i) {
+		if (str[i-1] == str[i]) {
+			// same character
+			++count;
+
+			if (count == 9) {
+				// limit achieved
+				oss << count << str[i];
+				count = 0;
+			} else if (i == (length - 1)) {
+				// special case when achieve last character
+				oss << count << str[i];
+			}
+		} else {
+			// different character
+			if (count > 0) {
+				oss << count << str[i-1];
+			}
+			count = 1;
+
+			if (i == (length - 1)) {
+				// special case when achieved last character
+				oss << count << str[i];
+			}
+		}
 	}
  
   return oss.str();
@@ -37,9 +69,10 @@ string runLengthEncoding(string str)
 int main()
 {
 	//string test{"AAAAAAAAAAAAABBCCCCDD"};
-	//string test{"aA"};
+	string test{"aA"};
 	
-	string test{"........______=========AAAA   AAABBBB   BBB"};
+	//string test{"........______=========AAAA   AAABBBB   BBB"};
+	//string test{" "};
 	cout << runLengthEncoding(test) << '\n';
 
 	return EXIT_SUCCESS;
