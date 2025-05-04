c324fadf7059f0d7890b03ab10bb8b5d16558bab
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 12:46:49 +0200

    Finish CaesarCypherEncryptor

--- C++/algoexpert/easy/CaesarCypherEncryptor/main.cpp
+++ C++/algoexpert/easy/CaesarCypherEncryptor/main.cpp
@@ -2,12 +2,26 @@
 
 using namespace std;
 
-string caesarCypherEncryptor(string str, int key) {
-  // Write your code here.
-  return "";
+string caesarCypherEncryptor(string str, int key) 
+{
+  string result{str};
+	char newChar;
+
+	for (auto& ch : result) {
+		newChar = ch + key;
+		if (newChar > 122) {
+			newChar = 96 + newChar % 122;
+		}
+
+		ch = newChar;
+	}
+
+  return result;
 }
 
 int main()
 {
+	cout << caesarCypherEncryptor("z", 1) << endl;
+
 	return EXIT_SUCCESS;
 }
