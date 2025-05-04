7e7fdb5610ae3a7519a29085a31ee6076280fa37
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 08:39:08 +0200

    Finish NthFibonacci

--- C++/algoexpert/easy/NthFibonacci/main.cpp
+++ C++/algoexpert/easy/NthFibonacci/main.cpp
@@ -4,11 +4,22 @@ using namespace std;
 
 int getNthFib(int n) 
 {
-  // Write your code here.
-  return -1;
+  if (n < 0) return -1; // error condition
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+
+  return getNthFib(n-1) + getNthFib(n-2);
 }
 
 int main()
 {
+
+	cout << "Fib(0) = " << getNthFib(0) << '\n';
+	cout << "Fib(1) = " << getNthFib(1) << '\n';
+	cout << "Fib(2) = " << getNthFib(2) << '\n';
+	cout << "Fib(3) = " << getNthFib(3) << '\n';
+	cout << "Fib(4) = " << getNthFib(4) << '\n';
+	cout << "Fib(5) = " << getNthFib(5) << '\n';
+
 	return EXIT_SUCCESS;
 }
