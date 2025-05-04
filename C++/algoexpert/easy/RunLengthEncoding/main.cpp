74f7e7317b0c1aaad7664c2f1cf1b0408531a06d
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-04 20:32:42 +0200

    Use ostringstream

--- C++/algoexpert/easy/RunLengthEncoding/main.cpp
+++ C++/algoexpert/easy/RunLengthEncoding/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -6,27 +7,27 @@ string runLengthEncoding(string str)
 {
 	char curr = str[0];
 	size_t length = str.size();
-	string result;
 	int count = 0;
+	ostringstream oss;
 
 	for (size_t i{1}; i < length; ++i) {
 		if (str[i] == curr) {
 			if (++count == 9) { // 9 is the max length
-				result += format("{}{}", count, curr);
+				oss << count << curr;
 				count = 1;
 			}
 
 			if (i == (length - 1)) {
-				result += format("{}{}", count, curr);		
+				oss << count << curr;		
 			}
 		} else {
-			result += format("{}{}", count, curr);
+			oss << count << curr;
 			count = 1;
 			curr = str[i];
 		}
 	}
  
-  return result;
+  return oss.str();
 }
 
 int main()
