5ae7df65285960bb311bc3064f6ed60c0f304503
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 12:00:27 +0200

    generateDocument function ended

--- C++/algoexpert/easy/GenerateDocument/main.cpp
+++ C++/algoexpert/easy/GenerateDocument/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <set>
 #include <iterator>
 
 using namespace std;
 
-bool generateDocument(string characters, string document) {
-  // Write your code here.
-  return false;
+bool generateDocument(string characters, string document) 
+{
+	unordered_map<char, size_t> frequencyCharsInDocument{};
+	for (char c : document) {
+		frequencyCharsInDocument[c]++;
+	}
+
+	unordered_map<char, size_t> frequencyChars{};
+	for (char c : characters) {
+		frequencyChars[c]++;
+	}
+	
+	for (const auto& pair : frequencyChars) {
+		auto it = frequencyCharsInDocument.find(pair.first);
+
+		if (it == frequencyCharsInDocument.end() 
+				|| pair.second < it->second) {
+			return false;
+		}
+	}
+
+  return true;
 }
 
 int main()
