abac3b330063e93f01195c44a47423d6008b6a20
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 11:28:54 +0200

    CommonCharacters end

--- C++/algoexpert/easy/CommonCharacters/main.cpp
+++ C++/algoexpert/easy/CommonCharacters/main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
+#include <iterator>
 
 using namespace std;
 
 vector<string> commonCharacters(vector<string> strings) {
-  // Write your code here.
-  return {};
+  vector<string> ret{};
+	map<char, size_t> mapChars{};
+	
+	for (const auto& s : strings) {
+		for (char c : s) {
+			mapChars.emplace(c, 0);
+		}			
+	}
+
+	for (const auto& s : strings) {
+		set<char> setOfChars{s.begin(), s.end()};
+		
+		for (char c : setOfChars) {
+				mapChars[c]++;
+		}
+	}
+
+	for (const auto& pair : mapChars) {
+		if (pair.second == strings.size()) {
+			ret.push_back(string{pair.first});
+		}
+	}
+
+	return ret;
 }
 
 int main()
 {
+	vector<string> strings{"abc", "bcd", "cbaccd"};
+	vector<string> result = commonCharacters(strings);
 
+	copy(result.begin(), result.end(), ostream_iterator<string>(cout, " "));
+	
 	return EXIT_SUCCESS;
 }
