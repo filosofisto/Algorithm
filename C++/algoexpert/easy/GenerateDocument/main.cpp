950e4862e83a384f8ed0fa581a7afa058846f588
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 12:13:19 +0200

    Finished

--- C++/algoexpert/easy/GenerateDocument/main.cpp
+++ C++/algoexpert/easy/GenerateDocument/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
-#include <set>
-#include <iterator>
 
 using namespace std;
 
-bool generateDocument(string characters, string document) 
+unordered_map<char, size_t> frequency(string text)
 {
-	unordered_map<char, size_t> frequencyCharsInDocument{};
-	for (char c : document) {
-		frequencyCharsInDocument[c]++;
+	unordered_map<char, size_t> result{};
+
+	for (char c : text) {
+		result[c]++;
 	}
 
-	unordered_map<char, size_t> frequencyChars{};
-	for (char c : characters) {
-		frequencyChars[c]++;
-	}
+	return result;
+}
+
+bool generateDocument(string characters, string document) 
+{
+	unordered_map<char, size_t> frequencyCharsInDocument = frequency(document);
+	unordered_map<char, size_t> frequencyChars = frequency(characters);
 	
 	for (const auto& pair : frequencyChars) {
 		auto it = frequencyCharsInDocument.find(pair.first);
@@ -32,6 +33,10 @@ bool generateDocument(string characters, string document)
 
 int main()
 {
+	string characters = "Bste!hetsi ogEAxperlrt x ";
+	string document   = "AlgoExpert is the Best!";
+
+	cout << boolalpha << generateDocument(characters, document) << endl;
 
 	return EXIT_SUCCESS;
 }
