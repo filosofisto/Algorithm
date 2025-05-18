5f917cc083f2126f905f42c86e3c2aec46a66327
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 12:37:14 +0200

    Fix

--- C++/algoexpert/easy/GenerateDocument/main.cpp
+++ C++/algoexpert/easy/GenerateDocument/main.cpp
@@ -19,11 +19,11 @@ bool generateDocument(string characters, string document)
 	unordered_map<char, size_t> frequencyCharsInDocument = frequency(document);
 	unordered_map<char, size_t> frequencyChars = frequency(characters);
 	
-	for (const auto& pair : frequencyChars) {
-		auto it = frequencyCharsInDocument.find(pair.first);
+	for (const auto& pair : frequencyCharsInDocument) {
+		auto it = frequencyChars.find(pair.first);
 
-		if (it == frequencyCharsInDocument.end() 
-				|| pair.second < it->second) {
+		if (it == frequencyChars.end() 
+				|| pair.second > it->second) {
 			return false;
 		}
 	}
@@ -33,8 +33,12 @@ bool generateDocument(string characters, string document)
 
 int main()
 {
-	string characters = "Bste!hetsi ogEAxperlrt x ";
-	string document   = "AlgoExpert is the Best!";
+	//string characters = "Bste!hetsi ogEAxperlrt x ";
+	//string document   = "AlgoExpert is the Best!";
+	string characters = "abcabc";
+	//string document   = "aabbccc";
+
+	string document = "";
 
 	cout << boolalpha << generateDocument(characters, document) << endl;
 
