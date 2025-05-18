70aaf1ded706e6603a192e6c7394a722bd1e87f6
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 15:06:41 +0200

    Finished

--- C++/algoexpert/easy/FirstNonRepeatingCharacter/main.cpp
+++ C++/algoexpert/easy/FirstNonRepeatingCharacter/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-int firstNonRepeatingCharacter(string string) {
-  // Write your code here.
-  return -1;
+int firstNonRepeatingCharacter(string str) {
+	map<char, size_t> m{};
+
+	for (char c : str) {
+		m[c]++;
+	}
+
+	size_t pos = -1;
+
+	for (const auto& pair : m) {
+		pos += pair.second;
+
+		if (pair.second == 1) {
+			return pos;
+		}
+	}
+
+	return -1;
 }
 
 int main()
 {
+	//string s{"abcdcaf"};
+	string s{"abbccaf"};
+	
+	cout << firstNonRepeatingCharacter(s) << '\n';
+
 	return EXIT_SUCCESS;
 }
