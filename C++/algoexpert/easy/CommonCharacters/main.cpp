2d1bd1eed7a7abff70c8ebc69e153e164e66eb35
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-18 10:30:33 +0200

    Common Characters start

---
+++ C++/algoexpert/easy/CommonCharacters/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> commonCharacters(vector<string> strings) {
+  // Write your code here.
+  return {};
+}
+
+int main()
+{
+
+	return EXIT_SUCCESS;
+}
