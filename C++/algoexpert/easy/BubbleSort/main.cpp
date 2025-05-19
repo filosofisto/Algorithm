b091659f0f422b03068d7b7536a35747a086c4c5
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-19 19:40:44 +0200

    Finish

--- C++/algoexpert/easy/BubbleSort/main.cpp
+++ C++/algoexpert/easy/BubbleSort/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
+
 using namespace std;
 
 vector<int> bubbleSort(vector<int> array) 
 {
-  // Write your code here.
-  return {};
+	vector<int> ret{array};
+	size_t limitOut = ret.size()-1;
+	size_t limitIn = ret.size();
+
+	for (size_t i{0}; i != limitOut; ++i) {
+		for (size_t j{i+1}; j < limitIn; ++j) {
+			if (ret[j] < ret[i]) {
+				swap(ret[j], ret[i]);
+			}
+		}
+	}
+
+  return ret;
 }
 
 int main()
 {
-
+	vector<int> array{9, 8, 7, 6, 5, 4, 3, 2, 1};
+	vector<int> result = bubbleSort(array);
+	copy(result.begin(), result.end(), ostream_iterator<int>{cout, " "});
+	
 	return EXIT_SUCCESS;
 }
 
