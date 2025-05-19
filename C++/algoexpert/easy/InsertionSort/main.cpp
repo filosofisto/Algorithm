35c861c8240c024be5081f29376f9fba47944b67
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-05-19 20:06:19 +0200

    Finish

--- C++/algoexpert/easy/InsertionSort/main.cpp
+++ C++/algoexpert/easy/InsertionSort/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
 vector<int> insertionSort(vector<int> array) 
 {
-  return {};
+	vector<int> result{array};
+	size_t limit{array.size()};
+	int key;
+	int j;
+
+	for (size_t i{1}; i < limit; ++i) {
+		key = result[i];
+		j = i - 1;
+
+		while (j >= 0 && result[j] > key) {
+			result[j+1] = result[j];
+			--j;
+		}
+
+		result[j+1] = key;	
+	}
+
+  return result;
 }
 
 int main()
 {
+	vector<int> array{9, 8, 7};
+	vector<int> result{insertionSort(array)};
+
+	copy(result.begin(), result.end(), ostream_iterator<int>{cout, " "});
+
 	return EXIT_SUCCESS;
 }
 
