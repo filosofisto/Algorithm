5692103ebe41f23e1be5330251d878ca28457603
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 17:20:18 +0200

    Fix

--- C++/algoexpert/medium/SpiralTraverse/main.cpp
+++ C++/algoexpert/medium/SpiralTraverse/main.cpp
@@ -63,13 +63,11 @@ int main()
 		{1}
 	};*/
 
-	{
-  		"array": [
-    				[1, 2, 3, 4],
-    				[10, 11, 12, 5],
-    				[9, 8, 7, 6]
-  			]
-	}
+	vector<vector<int>> matrix = {
+		{1, 2, 3, 4},
+		{10, 11, 12, 5},
+		{9, 8, 7, 6}
+	};
 
 	print(spiralTraverse(matrix));
 
