092f83a76175ea16c06aaecc93fc34c24a80bd41
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 17:10:00 +0200

    Finish

--- C++/algoexpert/medium/SpiralTraverse/main.cpp
+++ C++/algoexpert/medium/SpiralTraverse/main.cpp
@@ -4,10 +4,43 @@
 #include <algorithm>
 
 using namespace std;
+	
+vector<int> spiralTraverse(vector<vector<int>> array) 
+{
+	vector<int> ret{};
+	int startRow{0};
+	int endRow{static_cast<int>(array.size())-1};
+	int startCol{0};
+	int endCol{static_cast<int>(array[0].size())-1};
 
-vector<int> spiralTraverse(vector<vector<int>> array) {
-  // Write your code here.
-  return {};
+	while (startRow < endRow && startCol < endCol) {
+		// Left to Right Row
+		for (int c{startCol}; c <= endCol; ++c) {
+			ret.push_back(array[startRow][c]);
+		}
+
+		// Up to Down Col
+		for (int r{startRow+1}; r <= endRow; ++r) {
+			ret.push_back(array[r][endCol]);
+		}
+
+		// Right to Left Row
+		for (int c{endCol-1}; c >= startCol; --c) {
+			ret.push_back(array[endRow][c]);
+		}
+
+		// Down to Up Col
+		for (int r{endRow-1}; r > startRow; --r) {
+			ret.push_back(array[r][startCol]);
+		}
+
+		startRow++;
+		endRow--;
+		startCol++;
+		endCol--;
+	}
+
+  return ret;
 }
 
 void print(const vector<int> array)
@@ -17,6 +50,14 @@ void print(const vector<int> array)
 
 int main()
 {
+	vector<vector<int>> matrix = {
+		{1, 2, 3, 4},
+		{12, 13, 14, 5},
+		{11, 16, 15, 6},
+		{10, 9, 8, 7}
+	};
+
+	print(spiralTraverse(matrix));
 
 	return EXIT_SUCCESS;
 }
