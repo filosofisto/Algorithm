ae940c28e287cd3338dcc2619a4816d3fdd1d792
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 18:33:50 +0200

    Finish

--- C++/algoexpert/medium/ArrayOfProducts/main.cpp
+++ C++/algoexpert/medium/ArrayOfProducts/main.cpp
@@ -5,9 +5,28 @@
 
 using namespace std;
 
-vector<int> arrayOfProducts(vector<int> array) {
-  // Write your code here.
-  return {};
+int prod(vector<int> array, int pos) 
+{
+	int p{1};
+
+	for (int i{0}; i < array.size(); ++i) {
+		if (i != pos) {
+			p *= array[i];
+		}
+	}
+
+	return p;
+}
+
+vector<int> arrayOfProducts(vector<int> array) 
+{
+	vector<int> ret{};
+
+	for (int i{0}; i < array.size(); ++i) {
+		ret.push_back(prod(array, i));
+	}  
+
+	return ret;
 }
 
 void print(const vector<int> array)
@@ -17,8 +36,8 @@ void print(const vector<int> array)
 
 int main()
 {
-	vector<int> array{2, 1, 2, 2, 2, 3, 4, 2};
-	print(moveElementToEnd(array, 2));
+	vector<int> array{5, 1, 4, 2};
+	print(arrayOfProducts(array));
 
 	return EXIT_SUCCESS;
 }
