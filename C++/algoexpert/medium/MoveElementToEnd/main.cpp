c20009f44ab98ff30e7dcbdf8a69f5de16ac87c7
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 11:54:57 +0200

    Update main

--- C++/algoexpert/medium/MoveElementToEnd/main.cpp
+++ C++/algoexpert/medium/MoveElementToEnd/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+#include <vector>
+
 using namespace std;
+
+int getNextPos(vector<int> array, int toMove) 
+{
+  for (int i{array.size()-1}; i > -1; --i) {
+    if (array[i] != toMove) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+vector<int> moveElementToEnd(vector<int> array, int toMove) 
+{
+  int pos;
+  
+  for (int i{0}; i < array.size(); ++i) {
+    if (array[i] == toMove) {
+      pos = getNextPos(array, toMove);
+      swap(array[i], array[pos])
+    }
+  }
+  return array;
+}
 
 int main()
 {
