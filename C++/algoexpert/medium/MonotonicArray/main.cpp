dbd9e58a87c72abf51c199cab634aa197462fed8
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 12:51:23 +0200

    Add main.cpp

--- C++/algoexpert/medium/MonotonicArray/main.cpp
+++ C++/algoexpert/medium/MonotonicArray/main.cpp
@@ -5,37 +5,6 @@
 
 using namespace std;
 
-int getNextPos(vector<int> array, int toMove, int limit) 
-{
-	int pos{-1};
-
-  for (int i{static_cast<int>(array.size())-1}; i > -1 && i > limit; --i) {
-    if (array[i] != toMove) {
-      pos = i;
-			break;
-    }
-  }
-
-  return pos;
-}
-
-vector<int> moveElementToEnd(vector<int> array, int toMove) 
-{
-  int pos;
-  
-  for (int i{0}; i < array.size(); ++i) {
-    if (array[i] == toMove) {
-      pos = getNextPos(array, toMove, i);
-
-			if (pos == -1) {
-				break;
-			}
-
-      swap(array[i], array[pos]);
-    }
-  }
-  return array;
-}
 
 void print(const vector<int> array)
 {
@@ -44,8 +13,6 @@ void print(const vector<int> array)
 
 int main()
 {
-	vector<int> array{2, 1, 2, 2, 2, 3, 4, 2};
-	print(moveElementToEnd(array, 2));
 
 	return EXIT_SUCCESS;
 }
