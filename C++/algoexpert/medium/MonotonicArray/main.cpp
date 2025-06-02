585f833996f0acbab4f7c2f3605ae07b31817c69
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-06-02 13:10:30 +0200

    Finish

--- C++/algoexpert/medium/MonotonicArray/main.cpp
+++ C++/algoexpert/medium/MonotonicArray/main.cpp
@@ -5,6 +5,34 @@
 
 using namespace std;
 
+bool isIncreaseMonotonic(vector<int> array)
+{
+	int limit{static_cast<int>(array.size())-1};
+
+	for (int i{0}; i < limit; ++i) {
+		if (array[i] > array[i+1]) return false;
+	}
+
+	return true;
+}
+
+bool isDecreaseMonotonic(vector<int> array)
+{
+	int limit{static_cast<int>(array.size())-1};
+
+	for (int i{0}; i < limit; ++i) {
+		if (array[i] < array[i+1]) return false;
+	}
+
+	return true;
+}
+
+bool isMonotonic(vector<int> array)
+{
+	if (array.size() < 2) return true;
+
+	return isIncreaseMonotonic(array) || isDecreaseMonotonic(array);
+}
 
 void print(const vector<int> array)
 {
@@ -13,6 +41,23 @@ void print(const vector<int> array)
 
 int main()
 {
+	vector<int> array{-1, -5, -10, -1100, -1100, -1101, -1102, -9001};
+	cout << boolalpha << isMonotonic(array) << '\n';
 
+	vector<int> b{};
+	cout << isMonotonic(b) << '\n';
+
+	vector<int> c{1};
+	cout << isMonotonic(c) << '\n';
+
+	vector<int> d{1, 1, 1};
+	cout << isMonotonic(d) << '\n';
+
+	vector<int> e{1, 5, 10, 1100, 1101, 1102, 9001};
+	cout << isMonotonic(e) << '\n';
+
+	vector<int> f{1, 5, 2, 10, 1100, 1101, 1102, 9001};
+	cout << isMonotonic(f) << '\n';
+	
 	return EXIT_SUCCESS;
 }
